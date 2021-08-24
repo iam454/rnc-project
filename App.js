13aae463781fea75a38d79db14b0260481b2055c
iam454 <81221398+iam454@users.noreply.github.com>
2021-08-24 15:09:33 +0900

    Update App.js

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default class extends React.Component {
   };
   getWeather = async (latitude, longitude) => {
     const { data } = await axios.get(
-      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}$units=metric`
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
     );
     this.setState({ isLoading: false, temp: data.main.temp });
   };
