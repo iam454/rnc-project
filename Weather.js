71790c7d8320b1bd18af9757b5a1fbbc6a7d962f
iam454 <81221398+iam454@users.noreply.github.com>
2021-08-24 15:23:54 +0900

    Update App and Weather

--- Weather.js
+++ Weather.js
@@ -2,16 +2,35 @@ import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import PropTypes from "prop-types";
 
-export default function Weather({ temp }) {
+export default function Weather({ temp, condition }) {
   return (
     <View style={styles.container}>
       <Text>{temp}</Text>
+      <Text>{condition}</Text>
     </View>
   );
 }
 
 Weather.propTypes = {
   temp: PropTypes.number.isRequired,
+  condition: PropTypes.oneOf([
+    "Thunderstorm",
+    "Drizzle",
+    "Rain",
+    "Snow",
+    "Mist",
+    "Smoke",
+    "Haze",
+    "Dust",
+    "Fog",
+    "Sand",
+    "Dust",
+    "Ash",
+    "Squall",
+    "Tornado",
+    "Clear",
+    "Clouds",
+  ]).isRequired,
 };
 
 const styles = StyleSheet.create({
