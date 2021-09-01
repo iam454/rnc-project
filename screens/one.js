5bd7d0175ee97e6eb80819cb987cb30c93be5c71
kangsei <yhs7409@naver.com>
2021-09-02 01:38:28 +0900

    Commit

--- screens/one.js
+++
@@ -1,7 +0,0 @@
-// function One() {
-//     return (
-//       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-//         <Text>one!</Text>
-//       </View>
-//     );
-//   }
