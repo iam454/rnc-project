83fc87e574eb689991cee750c27b1b3fac16c5f8
kangsei <yhs7409@naver.com>
2021-11-09 00:59:35 +0900

    l

---
+++ melon.R
@@ -0,0 +1,22 @@
+library(httr)
+library(rvest)
+library(stringr)
+
+url = "https://www.melon.com/mymusic/dj/mymusicdjplaylistview_inform.htm?plylstSeq=496837151"
+get_url =GET(url)
+my_html = read_html(get_url,encoding = 'utf -8')
+song_name_p1= html_nodes(my_html,'.ellipsis.rank01')
+song_name_p2 =html_nodes(song_name_p1,'a')
+song_name = html_text(song_name_p2,trim =TRUE)
+
+singer_p1 = html_nodes(my_html,'.checkEllipsis')
+singer_name = html_text(singer_p1,trim =TRUE)
+
+song_list = character()
+singer_list = character()
+song_list = append(song_list,song_name)
+singer_list=append(singer_list,singer_name)
+
+df = data.frame(song_list,singer_list)
+write.csv(df , "sunnysong.csv",row.names=FALSE)
+rm(list=ls())
